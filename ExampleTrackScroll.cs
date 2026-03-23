ee43b8aa4776500fef39d6e0894dd9528c8d5e1b
deathwatchgaming <deathwatchgaming@gmail.com>
2026-03-23 00:50:38 -0700

    Update ExampleTrackScroll.cs

--- ExampleTrackScroll.cs
+++ ExampleTrackScroll.cs
@@ -10,14 +10,20 @@ using System.Collections.Generic;
 
 public class ExampleTrackScroll : MonoBehaviour
 {
-    // Create a List of MeshRenderers ie: trackRenderers and start with 2 empty track elements ie: [0,1]
+    [Header("Properties")]
+
+    [Tooltip("The track mesh renderers.")]
+    // Create a List of MeshRenderers ie: Track Renderers and start with 2 empty track elements ie: [0,1]
     [SerializeField] private List<MeshRenderer> _trackRenderers = new List<MeshRenderer>(new MeshRenderer[2]);
 
+    [Tooltip("The scroll speed.")]
     // Scroll the main texture based on time
     [SerializeField] private float scrollSpeed = 0.5f;
 
+    // The offset float
     private float offset;
 
+    // Update is called once per frame
     private void Update()
     {
         offset = Time.time * scrollSpeed;
